c6391046840809310d6ab41a9daa29d6a2eb8eb2
chywxsqy <chydxl123456@gmail.com>
2020-09-02 17:03:52 +0800

    test

---
+++ src/test.java
@@ -0,0 +1,10 @@
+/**
+ * @author xmr
+ * @date 2020/9/2 17:01
+ * @description
+ */
+public class test {
+    public static void main(String args[]){
+        System.out.println("Hi World");
+    }
+}
